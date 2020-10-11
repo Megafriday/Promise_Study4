189d5100664cdf476d9fd9b9c761ce89d0386a66
Megafriday <megafriday@yahoo.co.jp>
2020-10-11 15:40:02 +0900

    Update index7.js

--- index7.js
+++ index7.js
@@ -5,7 +5,7 @@ function promiseHello() {
 }
 
 promiseHello()
-	.then(str => str + "world")			// Promise.prototype.then() の戻り値は、Promise<any>
-	.then(data => console.log(data))	// Hello World と出力。 戻り値は、undefined
-	.then(data => console.log(data));	// undefined と出力
+	.then(str => str + "world")		    // Promise.prototype.then() の戻り値は、Promise<any>, 今回はPromise<String>
+	.then(data => console.log(data))	// Hello World と出力。 戻り値は、Promise<undefined>
+	.then(data => console.log(data));	// undefined と出力。　戻り値は、Promise<undefined>
 
