ca3d276e7a281448a9ed9ba6a4859c4a75879776
megafriday <megafriday@yahoo.co.jp>
2020-10-11 15:55:30 +0900

    change index8.js

--- index8.js
+++ index8.js
@@ -1,10 +1,11 @@
 // await は、Promiseにつけて同期させる。
 async function promiseFunc() {
-	await new Promise(resolve => setTimeout(() => {
-		console.log("３秒経過");
-		resolve();
-	}, 3000)); // 3秒待つ
-	console.log("func()内のコメント");
+	await new Promise(resolve => {   // await 付けた時、外した時で確かめる
+		setTimeout(() => {
+			console.log("３秒経過");
+			resolve();
+		}, 3000); // 3秒待つ
+	});
+	console.log("promiseFunc()内のコメント");
 }
-
 promiseFunc();
